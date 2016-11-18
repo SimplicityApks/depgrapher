dddd0fbf723d9b8ee6fd53c528fc193334b1a501
Patrick Mertens <patrick.mertens@rwth-aachen.de>
2016-11-18 14:30:57 +0100

    Add arrows to the dep tree printout

--- graph.go
+++ graph.go
@@ -211,6 +211,25 @@ func insertIntoString(s *string, offset int, insertion string) *string {
 
 var nodeFinished map[*Node]struct{}
 
+func printDepTreeArrows(line1, line2 *string, fieldstart, fieldlen int, midpoints ...int) (*string, *string) {
+	for _, midpoint := range midpoints {
+		// get the arrow direction (left, down, right)
+		// 							/       |      \
+		//                         V        V       V
+		if midpoint < fieldstart { // left
+			line1 = insertIntoString(line1, midpoint+2, "/")
+			line2 = insertIntoString(line2, midpoint+1, "V")
+		} else if midpoint < fieldstart+fieldlen { // down
+			line1 = insertIntoString(line1, midpoint, "|")
+			line2 = insertIntoString(line2, midpoint, "V")
+		} else { // right
+			line1 = insertIntoString(line1, midpoint-2, "\\")
+			line2 = insertIntoString(line2, midpoint-1, "V")
+		}
+	}
+	return line1, line2
+}
+
 func printDepTreeLevel(g *Graph, n *Node, out []*string, rightOffset int) (modout []*string, height int, width int) {
 	dependencies := g.GetDependencies(n)
 	name := " " + n.name + " "
@@ -229,37 +248,51 @@ func printDepTreeLevel(g *Graph, n *Node, out []*string, rightOffset int) (modou
 		}
 		// we are at the bottom of the graph
 		return out, 1, len(name)
-	} else {
-		if len(out) == 0 {
-			out = append(out, nil)
-		}
-		modout = out
-		out = out[1:]
-		// we have some dependencies, let us print them
-		var depWidth, locHeight, locWidth int
-		for _, dep := range dependencies {
-			out, locHeight, locWidth = printDepTreeLevel(g, dep, out, rightOffset+depWidth)
-			depWidth += locWidth
-			if height < locHeight {
-				height = locHeight
-			}
-		}
-		modout = append(modout[:1], out...)
-		// print our name in the middle
-		if len(name) > depWidth {
-			// shift everything below to the right
-			shiftOffsetLeft := strings.Repeat(" ", (len(name)-depWidth)/2)
-			shiftOffsetRight := strings.Repeat(" ", (len(name)-depWidth+1)/2)
-			for _, strptr := range out {
-				strptr = insertIntoString(insertIntoString(strptr, rightOffset+depWidth, shiftOffsetRight), rightOffset, shiftOffsetLeft)
-			}
-			modout[0] = insertIntoString(modout[0], rightOffset, name)
-			return modout, height, len(name)
-		} else {
-			modout[0] = insertIntoString(modout[0], rightOffset, strings.Repeat(" ", (depWidth-len(name))/2)+name+strings.Repeat(" ", (depWidth-len(name)+1)/2))
-			return modout, height, depWidth
+	}
+	// add two lines for the arrows
+	for len(out) < 3 {
+		out = append(out, nil)
+	}
+	modout = out
+
+	out = out[3:]
+	// we have some dependencies, let us print them
+	var depWidth, locHeight, locWidth int
+	// save the midpoints to connect the arrows to
+	depMids := make([]int, len(dependencies))
+	for i, dep := range dependencies {
+		out, locHeight, locWidth = printDepTreeLevel(g, dep, out, rightOffset+depWidth)
+		depMids[i] = rightOffset + depWidth + locWidth/2
+		depWidth += locWidth
+		if height < locHeight {
+			height = locHeight
 		}
 	}
+
+	modout = append(modout[:3], out...)
+	// print our name in the middle
+	if len(name) > depWidth {
+		// shift everything below to the right
+		offsetLeft := (len(name) - depWidth) / 2
+		offsetRight := (len(name) - depWidth + 1) / 2
+		for _, strptr := range out {
+			strptr = insertIntoString(insertIntoString(strptr, rightOffset+depWidth, strings.Repeat(" ", offsetRight)), rightOffset, strings.Repeat(" ", offsetLeft))
+		}
+		// same for the mid points
+		for i, mid := range depMids {
+			if mid < rightOffset+len(name)/2 {
+				depMids[i] += offsetLeft
+			} else {
+				depMids[i] += offsetRight
+			}
+		}
+		modout[1], modout[2] = printDepTreeArrows(modout[1], modout[2], rightOffset, len(name), depMids...)
+	} else {
+		modout[1], modout[2] = printDepTreeArrows(modout[1], modout[2], rightOffset+(depWidth-len(name))/2, len(name), depMids...)
+		name = strings.Repeat(" ", (depWidth-len(name))/2) + name + strings.Repeat(" ", (depWidth-len(name)+1)/2)
+	}
+	modout[0] = insertIntoString(modout[0], rightOffset, name)
+	return modout, height, len(name)
 }
 
 // printDepTree pretty prints the dependency tree of the specified startNode to stdout
